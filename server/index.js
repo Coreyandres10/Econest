cba8c861ccd0931c804d64551c8f699d57c74eb3
Corey Cevallos <coreyandres10@gmail.com>
2024-05-02 12:48:27 -0400

    Coreys Changes Apr 30 11:50 pm

--- server/index.js
+++ server/index.js
@@ -1,175 +1,329 @@
-// Importing necessary modules and libraries
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require('path');
-const fs = require('fs');
-const csvmulter = require('./util/csv_handler'); 
+const path=require('path')
+const fs=require('fs')
+const csvmulter=require('./util/csv_handler')
 const EconestModel = require("./models/Econest");
-const csv = require('csvtojson');
-const stockmodel = require('./models/stock');
+const csv=require('csvtojson')
+const stockmodel=require('./models/stock')
 const app = express();
-const transactionmodel = require('./models/Transaction');
-const stockClosePriceModel = require('./models/stockcloseprice');
-
-// Middleware setup
+const transactionmodel=require('./models/Transaction')
+const stockClosePriceModel=require('./models/stockcloseprice')
 app.use(express.json());
 app.use(cors());
 
-// MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/Econest");
 
-// CSV Upload endpoint
+//-----------------------------------------------------------------------------
+//LOGIN
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    EconestModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    res.status(200).json({ status: "success", message: "Login successful" });
+                } else {
+                    res.status(401).json({ status: "error", message: "Incorrect password" });
+                }
+            } else {
+                res.status(404).json({ status: "error", message: "User not found" });
+            }
+        })
+        .catch(err => {
+            console.error("Login error:", err);
+            res.status(500).json({ status: "error", message: "An error occurred while logging in" });
+        });
+});
+
+app.post('/register', (req, res) => {
+    EconestModel.create(req.body)
+        .then(customers => res.status(201).json(customers))
+        .catch(err => res.status(500).json({ status: "error", message: err.message }));
+});
+
+//-----------------------------------------------------------------------------
+//CSV
+
 app.post('/uploadcsv', csvmulter.single("file"), async (req, res) => {
     try {
-        // Directory path for saving uploaded CSV files
-        const dirpath = path.join(__dirname, 'public', 'csv');
+        const dirpath = path.join(__dirname, 'public', 'csv'); 
 
-        // Creating directory if not exists
+      
         if (!fs.existsSync(dirpath)) {
             fs.mkdirSync(dirpath, { recursive: true });
         }
 
-        // Extracting CSV data from request buffer
-        const csvData = req.file.buffer.toString();
+        const csvData = req.file.buffer.toString(); 
         const destinationFilePath = path.join(dirpath, req.file.originalname);
 
-        // Writing CSV data to file
         fs.writeFileSync(destinationFilePath, csvData);
-
-        // Converting CSV data to JSON and saving to MongoDB
-        csv().fromFile(destinationFilePath).then(async (response) => {
-            response.map(async (val, i) => {
-                // Extracting CSV data fields
-                const transactionDetail = val['Transaction Detail'];
-                const date = val.Date;
-                const amount = val.Amount;
-                const balance = val.Balance;
-                const reoccuring = val.Reoccuring;
-                const type = val.type;
-
-                // Checking if all required fields are present
-                if (transactionDetail && date && amount && balance && reoccuring && type) {
-                    await transactionmodel.create({
-                        Date: date,
-                        Transaction_Detail: transactionDetail,
-                        Amount: amount,
-                        Balance: balance,
-                        Reoccuring: reoccuring,
-                        type: type
-                    });
-                }
-            });
+csv().fromFile(destinationFilePath).then(async(response)=>{
+  response.map(async(val,i)=>{
+    const transactionDetail = val['Transaction Detail'];
+    const date=val.Date
+    const amount=val.Amount;
+    const balance=val.Balance;
+    const reoccuring=val.Reoccuring
+    const type=val.type
+if(transactionDetail && date && amount && balance && reoccuring && type){
+    console.log(type)
+    console.log(balance)
+    console.log(amount)
+await transactionmodel.create({
+    Date:date,
+    Transaction_Detail:transactionDetail,
+    Amount:amount,
+    Balance:balance,
+    Reoccuring:reoccuring,
+    type:type
+})
+}
+  })
+  
+})
+      return  res.status(200).json({
+            message: "File uploaded successfully"
         });
-
-        // Sending success response
-        return res.status(200).json({ message: "File uploaded successfully" });
     } catch (error) {
         console.error(error.message);
-        // Sending error response
-        res.status(500).json({ error: "Server error. Please try again later." });
+        res.status(500).json({
+            error: "Server error. Please try again later."
+        });
     }
 });
 
-// ----------------------------------------------------------
+//-----------------------------------------------------------------------------
+//GET DATA
 
-// GET DATA endpoints
-// Endpoint to get transaction data
-app.get('/get-transaction', async (req, res) => {
-    try {
-        // Fetching transaction data from MongoDB
-        let response = await transactionmodel.find({});
-        // Sending response
-        return res.status(200).json({ response });
-    } catch (error) {
-        // Handling errors
-        res.status(500).json({ error: "Server error. Please try again later." });
+app.get('/get-transaction',async(req,res)=>{
+    try{
+let response=await transactionmodel.find({})
+return res.status(200).json({
+    response
+})
+
+    }catch(error){
+        res.status(500).json({
+            error: "Server error. Please try again later."
+        });
     }
-});
+})
 
-// Endpoint to get stock data
-app.get('/get-stocks', async (req, res) => {
-    try {
-        // Fetching stock data from MongoDB
-        let response = await stockmodel.find({});
-        // Sending response
-        return res.status(200).json({ response });
-    } catch (error) {
-        // Handling errors
-        res.status(500).json({ error: "Server error. Please try again later." });
+app.get('/get-stocks',async(req,res)=>{
+    try{
+let response=await stockmodel.find({})
+return res.status(200).json({
+    response
+})
+
+    }catch(error){
+        res.status(500).json({
+            error: "Server error. Please try again later."
+        });
     }
-});
+})
 
-// Endpoint to get stock close prices
 app.get('/get-stock-close-prices', async (req, res) => {
     try {
-        // Fetching stock close prices data from MongoDB
-        const stockClosePrices = await stockClosePriceModel.find();
-        // Sending response
-        return res.status(200).json(stockClosePrices);
+      // Assuming stockClosePriceModel is your Mongoose model for stock close prices
+      const stockClosePrices = await stockClosePriceModel.find();
+      return res.status(200).json(stockClosePrices);
     } catch (error) {
-        // Handling errors
-        console.error("Error fetching stock close prices:", error); // Log error
-        return res.status(500).json({ error: 'Server error. Please try again later.' });
+      console.error("Error fetching stock close prices:", error); // Log error
+      return res.status(500).json({ error: 'Server error. Please try again later.' });
+    }
+  });
+
+//-----------------------------------------------------------------------------
+//INCOME
+
+app.post('/insert-income',async(req,res)=>{
+    try{
+let {amount,source,category}=req.body;
+await transactionmodel.create({
+  type:category,
+  Amount:amount,
+  Transaction_Detail:source+' ',
+})
+return res.status(200).json({
+    message:"success"
+})
+    }catch(e){
+        console.log(e.message)
+        return res.status(40).json(
+          {
+            error:"Server error please try later"
+          }
+        )
     }
 });
 
-// ----------------------------------------------------------
+app.put('/update-income/:id', async (req, res) => {
+    const { id } = req.params;
+    const { amount, source, category } = req.body;
+    const income = await transactionmodel.findById(id);
 
-// INCOME endpoints
-// Endpoint to insert income
-app.post('/insert-income', async (req, res) => {
+    income.Amount = amount;
+    income.Transaction_Detail = source;
+    income.type = category;
+
+    await income.save();
+
+    return res.status(200).json({ message: 'Expense updated successfully' });
+});
+
+app.delete('/delete-income/:id', async (req, res) => {
+    const { id } = req.params;
+    const result = await transactionmodel.findByIdAndDelete(id);
+    
+    if (result) {
+        return res.status(200).json({ message: 'Expense deleted successfully' });
+    } else {
+        return res.status(500).json({ error: 'Failed to delete expense' });
+    }
+});
+
+//-----------------------------------------------------------------------------
+//EXPENSES
+
+app.post('/insert-expense',async(req,res)=>{
+    try{
+let {amount,source,category}=req.body;
+await transactionmodel.create({
+  type:category,
+  Amount:amount,
+  Transaction_Detail:source+' ',
+})
+return res.status(200).json({
+    message:"success"
+})
+    }catch(e){
+        console.log(e.message)
+        return res.status(40).json(
+          {
+            error:"Server error please try later"
+          }
+        )
+    }
+});
+
+app.put('/update-expense/:id', async (req, res) => {
+    const { id } = req.params;
+    const { amount, source, category } = req.body;
+    const expense = await transactionmodel.findById(id);
+
+    expense.Amount = amount;
+    expense.Transaction_Detail = source;
+    expense.type = category;
+
+    await expense.save();
+
+    return res.status(200).json({ message: 'Expense updated successfully' });
+});
+
+
+app.delete('/delete-expense/:id', async (req, res) => {
+    const { id } = req.params;
+    const result = await transactionmodel.findByIdAndDelete(id);
+    
+    if (result) {
+        return res.status(200).json({ message: 'Expense deleted successfully' });
+    } else {
+        return res.status(500).json({ error: 'Failed to delete expense' });
+    }
+});
+
+//-----------------------------------------------------------------------------
+//STOCK
+
+app.post('/insert-stock',async(req,res)=>{
+    let { stock_symbol,buy_price, buy_date, shares}=req.body;
+    try{
+        let response=await stockmodel.create({
+stock_symbol,
+buy_price,
+buy_date,
+shares
+
+        })
+        return res.status(200).json({
+            response
+        })
+    }catch(e){
+        console.log(e.message)
+        return res.status(40).json(
+          {
+            error:"Server error please try later"
+          }
+        )
+    }
+})
+
+app.put('/update-stock/:id', async (req, res) => {
+    const { id } = req.params;
+    const { stock_symbol, buy_price, buy_date, shares} = req.body; // Include 'shares' in destructuring
+
     try {
-        // Extracting data from request body
-        let { amount, source, category } = req.body;
-        // Creating new income transaction
-        await transactionmodel.create({
-            type: category,
-            Amount: amount,
-            Transaction_Detail: source + ' ',
-        });
-        // Sending success response
-        return res.status(200).json({ message: "success" });
-    } catch (e) {
-        console.log(e.message);
-        // Sending error response
-        return res.status(500).json({ error: "Server error please try later" });
+        const stock = await stockmodel.findById(id);
+
+        // Update stock data
+        stock.stock_symbol = stock_symbol;
+        stock.buy_price = buy_price;
+        stock.buy_date = buy_date;
+        stock.shares = shares; // Update shares
+
+        await stock.save();
+
+        return res.status(200).json({ message: 'Stock updated successfully' });
+    } catch (error) {
+        console.error('Error updating stock:', error);
+        return res.status(500).json({ error: 'Internal server error' });
     }
 });
 
-// ----------------------------------------------------------
 
-// STOCK endpoints
-// Endpoint to insert stock
-app.post('/insert-stock', async (req, res) => {
+
+app.delete('/delete-stock/:id', async (req, res) => {
+    const { id } = req.params;
+    const result = await stockmodel.findByIdAndDelete(id);
+    
+    if (result) {
+        return res.status(200).json({ message: 'Stock deleted successfully' });
+    } else {
+        return res.status(500).json({ error: 'Failed to delete stock' });
+    }
+});
+
+app.post('/insert-stock-close-price', async (req, res) => {
+    const { stock_symbol, buy_price, buy_date, shares, previousClose} = req.body;
     try {
-        // Extracting data from request body
-        let { stock_symbol, buy_price, buy_date, shares } = req.body;
-        // Creating new stock entry
-        let response = await stockmodel.create({
-            stock_symbol,
-            buy_price,
-            buy_date,
-            shares
-        });
-        // Sending success response
-        return res.status(200).json({ response });
-    } catch (e) {
-        console.log(e.message);
-        // Sending error response
-        return res.status(500).json({ error: "Server error please try later" });
+      console.log("Received request to insert stock close price:", req.body);
+      const response = await stockClosePriceModel.create({
+        stock_symbol,
+        buy_price,
+        buy_date,
+        shares,
+        previousClose,
+      });
+      console.log("Inserted stock close price successfully:", response);
+      return res.status(200).json({ response });
+    } catch (error) {
+      console.error("Error inserting stock close price:", error); // Log error
+      return res.status(500).json({ error: 'Server error. Please try again later.' });
     }
-});
+  });
 
-// ----------------------------------------------------------
+//-----------------------------------------------------------------------------
+//TEST CONNECTION
 
-// Testing connection middleware
 app.use((err, req, res, next) => {
     console.error("Server error:", err);
     res.status(500).json({ status: "error", message: "Internal server error" });
 });
 
-// Start server
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
 });
